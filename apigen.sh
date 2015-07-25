b0667d297d38ffe8999bec76a61c5c2fb0a5de9b
Daniele Scasciafratte <mte90net@gmail.com>
2015-07-25 15:05:58 +0200

    Update apigen.sh

--- apigen.sh
+++ apigen.sh
@@ -7,5 +7,3 @@ apigen --source=./ --exclude=*CMB*,*WP-Admin-Notice*,*WP-Contextual-Help*,*CPT_C
 
 echo "#menu > span:first-child, #menu > a:first-child span {background: url('https://raw.githubusercontent.com/Mte90/WordPress-Plugin-Boilerplate-Powered/master/plugin-name/assets/icon-128x128.png');content: "";height: 128px;width: 128px;display:block;}" >> ./resources/style.css
 echo "#menu > span:first-child, #menu > a:first-child, #menu > a:first-child:hover {background-color:transparent;color: transparent;}" >> ./resources/style.css
-
-rm -r plugin-name/

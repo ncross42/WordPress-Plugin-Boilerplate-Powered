1d47ccc44e13b085197677ec41f620442f883eba
Daniele Scasciafratte <mte90net@gmail.com>
2015-07-25 14:50:35 +0200

    update apigen syntax

--- apigen.sh
+++ apigen.sh
@@ -3,7 +3,7 @@ set -e
 
 slug="plugin-name"
 
-apigen --source ./ --exclude=*CMB*,*WP-Admin-Notice*,*WP-Contextual-Help*,*CPT_Core*,*Taxonomy_Core*,*Widget-Boilerplate* --destination ./ --title "Wordpress Plugin Boilerplate Powered" > /dev/null
+apigen --source=./ --exclude=*CMB*,*WP-Admin-Notice*,*WP-Contextual-Help*,*CPT_Core*,*Taxonomy_Core* --destination=./ --title="Wordpress Plugin Boilerplate Powered" > /dev/null
 
 echo "#menu > span:first-child, #menu > a:first-child span {background: url('https://raw.githubusercontent.com/Mte90/WordPress-Plugin-Boilerplate-Powered/master/plugin-name/assets/icon-128x128.png');content: "";height: 128px;width: 128px;display:block;}" >> ./resources/style.css
 echo "#menu > span:first-child, #menu > a:first-child, #menu > a:first-child:hover {background-color:transparent;color: transparent;}" >> ./resources/style.css
